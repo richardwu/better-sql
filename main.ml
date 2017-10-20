57015d9f5232f4ddb3bcb84280aec6b8c7427c62
Richard Wu <richardwu1997@gmail.com>
2017-10-20 18:15:00 -0400

    added more basic pushdown examples

--- main.ml
+++ main.ml
@@ -1,10 +1,24 @@
-let q = SELECT(
-  PROJECT(
-    Compare(Eq, VAR(Table "a"), CONST(1)),
-    JOIN(
-      Inner, Compare(Eq, VAR(Table "a"), VAR(Table "b")), SCAN(Table "a"), SCAN(Table "b")
-    )
-  )
-)
+let basicQuery (pred:predicate) (joinExpr:expr) =
+  SELECT(PROJECT(pred, joinExpr))
 
-let qpushed = pushdown q None
+let basicComp (table:string) (const:int) =
+  Compare(Eq, VAR(Table table), CONST(const))
+
+let basicJoin (tableA:string) (tableB:string) =
+  JOIN(Inner, Compare(Eq, VAR(Table tableA), VAR(Table tableB)), SCAN(Table tableA), SCAN(Table tableB))
+
+(* SELECT * FROM a JOIN b WHERE a.X = 1 *)
+let q1 = basicQuery (basicComp "a" 1) (basicJoin "a" "b")
+(* SELECT * FROM (a : a.X = 1) JOIN (b : b.X = 1) WHERE a.X = 1 *)
+let q1pushed = pushdown q1 None
+
+(* SELECT * FROM a JOIN b WHERE b.X = 1 *)
+let q2 = basicQuery (basicComp "b" 1) (basicJoin "a" "b")
+(* SELECT * FROM (a : a.X = 1) JOIN (b : b.X = 1) WHERE b.X = 1 *)
+let q2pushed = pushdown q2 None
+
+(* SELECT * FROM b JOIN c WHERE a.X = 1 ; this not valid but simply to test pushdown *)
+let q3 = basicQuery (basicComp "a" 1) (basicJoin "b" "c")
+(* SELECT * FROM b JOIN c WHERE a.X = 1 *)
+let q3pushed = pushdown q3 None
+
